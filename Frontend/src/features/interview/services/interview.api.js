9b220059b801e2186de09a43817fa7d2fb42e80c
Adarsh Shrivastav <adarshshrivastav08@gmail.com>
2026-07-02 11:03:58 +0530

    update API base URL to localhost for local development

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://interview-plan0-2-git-main-adrsri001s-projects.vercel.app/",
+    baseURL: "http://localhost:3000",
     withCredentials: true,
 })
 
